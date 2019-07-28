46688f4fbbe219b3fce65a73ae62fd8f34b350c8
Andrew Horn <arhorn@smcm.edu>
2019-07-27 21:21:48 -0500

    corrected error where firstTime card always loads, refactored

--- src/container/App/getFirstLoad.js
+++ src/container/App/getFirstLoad.js
@@ -2,10 +2,7 @@ import getLocalStorage from './localStorageFunctions/getLocalStorage';
 
 const getFirstLoad = () => {
   let isFirstLoad;
-  if (typeof window !== `undefined`) {
-    // for `gatsby build` to succeed
-    isFirstLoad = getLocalStorage('isFirstLoad');
-  }
+  isFirstLoad = getLocalStorage('isFirstLoad');
   if (typeof isFirstLoad === 'boolean' && isFirstLoad === false) {
     return false;
   }
--- src/container/App/localStorageFunctions/getLocalStorage.js
+++ src/container/App/localStorageFunctions/getLocalStorage.js
@@ -1,9 +1,6 @@
 const getLocalStorage = itemName => {
   if (typeof window !== `undefined`) {
     // for `gatsby build` to succeed
-    if (!JSON.parse(localStorage.getItem(itemName))) {
-      return;
-    }
     return JSON.parse(localStorage.getItem(itemName));
   }
   return;
