25f6aed9fb429e7d4b27e3698e725b38fc3e431b
Andrew Horn <arhorn@smcm.edu>
2019-07-27 21:50:58 -0500

    put MainView in its own component

--- src/presentational/AppView/AppView.js
+++ src/presentational/AppView/AppView.js
@@ -1,57 +1,15 @@
 import React from 'react';
-import { Spinner } from 'reactstrap';
 
 import FirstLoadCard from '../FirstLoadCard/FirstLoadCard';
-import PlaceList from '../PlaceList/PlaceList';
-import PersonalFilters from '../PersonalFilters/PersonalFilters';
-import SortBy from '../SortBy/SortBy';
-import ServicesFilter from '../ServicesFilter/ServicesFilter';
-import CollapseFiltersButton from '../CollapseFiltersButton/CollapseFiltersButton';
 
-const AppView = ({
-  isFirstLoad,
-  filters,
-  filters: { service },
-  sortBy,
-  displayedPlaceList,
-  currentPosition,
-  shareLocationClicked,
-  onChooseFilter,
-  onChooseSortBy,
-  toggleCollapseFilters,
-  travelTimesFinishedLoading
-}) => (
+import MainView from '../MainView/MainView';
+
+const AppView = ({ isFirstLoad, shareLocationClicked, ...props }) => (
   <>
     {isFirstLoad ? (
       <FirstLoadCard onClick={shareLocationClicked} />
     ) : (
-      <>
-        <CollapseFiltersButton
-          collapsed={filters.collapsed}
-          toggleCollapseFilters={toggleCollapseFilters}
-        />
-        <br />
-        <br />
-        {filters.collapsed || (
-          <PersonalFilters filters={filters} onChooseFilter={onChooseFilter} />
-        )}
-        <>
-          <ServicesFilter selected={service} onChooseFilter={onChooseFilter} />
-          <br />
-          <SortBy sortBy={sortBy} onChooseSortBy={onChooseSortBy} />
-          <br />
-        </>
-        {travelTimesFinishedLoading ? (
-          <PlaceList
-            displayedPlaceList={displayedPlaceList}
-            currentPosition={currentPosition}
-          />
-        ) : (
-          <>
-            <Spinner style={{ marginLeft: '10em' }} color="purple" />
-          </>
-        )}
-      </>
+      <MainView {...props} />
     )}
   </>
 );
