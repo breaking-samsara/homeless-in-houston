08538ffe9fa309a33e856834ef9952a2d119c54d
Andrew Horn <arhorn@smcm.edu>
2019-07-27 21:38:35 -0500

    updated collapse filters button text, made it dynamic

--- src/presentational/CollapseFiltersButton/CollapseFiltersButton.js
+++ src/presentational/CollapseFiltersButton/CollapseFiltersButton.js
@@ -3,7 +3,9 @@ import { Button } from 'reactstrap';
 
 const CollapseFiltersButton = ({ collapsed, toggleCollapseFilters }) => (
   <>
-    <Button onClick={() => toggleCollapseFilters()}>collapse</Button>
+    <Button onClick={() => toggleCollapseFilters()}>
+      {collapsed ? 'See your details' : 'Hide your details'}
+    </Button>
     <br />
     <br />
   </>
