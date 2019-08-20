f82c3ba56ab9065ba9ab37004868417d9eab2b64
Andrew Horn <arhorn@smcm.edu>
2019-08-20 00:28:45 -0500

    added more shelters

--- src/container/App/getInitialUnfilteredPlaceList/shelterList.js
+++ src/container/App/getInitialUnfilteredPlaceList/shelterList.js
@@ -68,6 +68,116 @@ const shelterList = [
     pets: 'not okay',
     veteran: 'specific',
     services: ['day shelter']
+  },
+  {
+    name: "Houston Area Women's Center",
+    gender: ['female'],
+    age: ['all'],
+    family: 'okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'SEARCH Homeless Services',
+    gender: ['all'],
+    age: ['all'],
+    family: 'okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Magnificat Houses',
+    gender: ['all'],
+    age: ['all'],
+    family: 'not okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Salvation Army Center of Hope',
+    gender: ['all'],
+    age: ['all'],
+    family: 'okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Doris and Carlos Morris Mens Development Center',
+    gender: ['nonbinary', 'male'],
+    age: ['all'],
+    family: 'not okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter', 'day shelter']
+  },
+  {
+    name: "Miryam's Hostel - Loaves and Fishes",
+    gender: ['nonbinary', 'female'],
+    age: ['all'],
+    family: 'okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Star of Hope - Women & Family Emergency Shelter',
+    gender: ['nonbinary', 'female'],
+    age: ['all'],
+    family: 'specific',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Madge Bush Transitional Living Center',
+    gender: ['nonbinary', 'female'],
+    age: ['all'],
+    family: 'okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'The Life Center for the Homeless',
+    gender: ['all'],
+    age: ['all'],
+    family: 'not okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter', 'day shelter']
+  },
+  {
+    name: 'Modest Family Health Care Center',
+    gender: ['all'],
+    age: ['all'],
+    family: 'not okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
+  },
+  {
+    name: 'Turning Point Center',
+    gender: ['all'],
+    age: ['65 and over'], // really 50+
+    family: 'not okay',
+    lgbt: 'okay',
+    pets: 'not okay',
+    veteran: 'okay',
+    services: ['night shelter']
   }
 ];
 
